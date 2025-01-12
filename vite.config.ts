a8f1549cde01c5ac0a941887e253f16234fc7986
Priyanshu <142545999+kumarpriyanshu09@users.noreply.github.com>
2025-01-11 23:11:23 -0500

    minor changes in the vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/get2knowPriyanshu/'
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
